091bbc8d67c2ed83725365ce7668d3868af92a8e
fukudamax <maxgainbooster@gmail.com>
2019-09-16 22:34:36 +0900

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 // 課題1: クイズデータをAPI経由で取得するクラスを作成する
 // - クラス名はQuizFetcherクラスとする
+
+const END_POINT = 'https://opentdb.com/api.php?amount=10';
+
+class QuizFetcher{
+    static fetchMultipleQuizData(){
+        const url = `${END_POINT}&type=multiple`;
+        return fetch(url).then(response => response.json())
+    }
+
+    static fetchTrueOrFalseQuizData(){
+        const url = `${END_POINT}&type=boolean`;
+        return fetch(url).then(response => response.json())    
+    }    
+    
+}
+
+QuizFetcher.fetchMultipleQuizData().then(data => {
+    console.log('課題の解答は',data.results)
+})
+
+
+QuizFetcher.fetchTrueOrFalseQuizData().then(data => {
+    console.log('課題の解答は',data.results)
+})
 // - 次のクラスメソッドを作成する
 //   - fetchMultipleQuizData : 4択問題のクイズのみをAPI経由で取得する
 //   - fetchTrueOrFalseQuizData : 2択問題のクイズのみをAPI経由で取得する
@@ -13,7 +37,9 @@
 
 
 // 課題2: fetchMultipleQuizDataメソッドを実行してAPIの結果に含まれるresultsプロパティの値をconsole.logで出力する
+console.log(QuizFetcher.fetchMultipleQuizData())
 
 
 // 課題3: fetchTrueOrFalseQuizDataメソッドを実行してAPIの結果に含まれるresultsプロパティの値をconsole.logで出力する
+console.log(QuizFetcher.fetchTrueOrFalseQuizData())
 
